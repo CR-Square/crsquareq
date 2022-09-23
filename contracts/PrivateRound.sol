dba716cc491ad51b661bf385b9d7f9f6ba57de8c
agiratech-leojames <leojames@agiratech.com>
2022-09-23 15:44:13 +0530

    optimization done to the PrivateRound

--- contracts/PrivateRound.sol
+++ contracts/PrivateRound.sol
@@ -50,18 +50,9 @@ contract InvestorLogin{
 
 contract PrivateRound{
 
-    mapping(address => mapping(uint => MilestoneSetup[])) private _milestone;
-    // sets investor address to mileStones created by the founder.
-    mapping(address => mapping(uint => address)) private addressIdToAddress;    
-    // founder => roundId => investor
-    mapping(uint => mapping(address => uint)) public fundsRequested;   
-    // round id => funds => investor
-    mapping(uint => mapping(address => uint)) public initialPercentage;
-    // round id => investor => initialPercentage
-    mapping(uint => mapping(address => address)) public tokenExpected;
-    // roundid => investor => tokenaddress.
-    mapping(uint => mapping(address => address)) public seperateContractLink;
-    // round id => founder => uinstance contract address.
+    mapping(address => mapping(uint => MilestoneSetup[])) private _milestone; // sets investor address to mileStones created by the founder.
+    mapping(uint => mapping(address => uint)) public initialPercentage;  // round id => investor => initialPercentage   
+    mapping(uint => mapping(address => address)) public seperateContractLink;  // round id => founder => uinstance contract address.   
     mapping(uint => bool) private roundIdControll;
 
     struct MilestoneSetup {
@@ -86,15 +77,9 @@ contract PrivateRound{
         * WRITE FUNCTIONS:
     */
 
-    mapping(address => MilestoneSetup) public dates;
-
-    function createPrivateRound(address _founder, uint _roundId, address _tokenContract, 
-    address _investorSM, uint _fundRequested, uint _initialPercentage, 
-    MilestoneSetup[] memory _mile) public {
+    function createPrivateRound(uint _roundId, address _investorSM, uint _initialPercentage, MilestoneSetup[] memory _mile) public {
         require(msg.sender != address(0), "The address is not valid or the address is 0");
-        // FounderLogin founder = FounderLogin(_founderSM);
         InvestorLogin investor = InvestorLogin(_investorSM);
-        // require(founder.verifyFounder(msg.sender) == true, "The address is not registered in the 'FounderLogin' contract");
         require(investor.verifyInvestor(msg.sender) == true, "The address is not registered in the 'InvestorLogin' contract");
         if(roundIdControll[_roundId] == true){
             revert("round Id is already taken");
@@ -105,26 +90,14 @@ contract PrivateRound{
             milestoneWithdrawalStatus[_roundId][_mile[i]._num] = false;
         }
         roundIdControll[_roundId] = true;
-        addressIdToAddress[_founder][_roundId] = msg.sender;
-        fundsRequested[_roundId][msg.sender] = _fundRequested;
         initialPercentage[_roundId][msg.sender] = _initialPercentage;
-        tokenExpected[_roundId][msg.sender] = _tokenContract;
     }
 
-    // initial90
-    // initial10
-    // escrowBalance
-
-    mapping(uint => mapping(address => uint)) public remainingTokensOfInvestor;
-    // round id => investor => tokens
-    mapping(uint => mapping(address => uint)) public totalTokensOfInvestor;
-    mapping(uint => mapping(address => uint)) public initialTokensForFounder;
-    // round id => investor => tokens
-    uint public escrowBalance;
-    // round id => investor => tokens
+    mapping(uint => mapping(address => uint)) public remainingTokensOfInvestor;  // round id => investor => tokens   
+    mapping(uint => mapping(address => uint)) public totalTokensOfInvestor;    // round id => investor => tokens 
+    mapping(uint => mapping(address => uint)) public initialTokensForFounder;  // round id => founder => tokens
     mapping(uint => mapping(address => bool)) private initialWithdrawalStatus;
     mapping(uint => address) private contractAddress;
-
 
     function depositTokens(address _tokenContract, address _investorSM, address _founder, uint _tokens, uint _roundId) public {
         require(msg.sender != address(0), "The address is not valid or the address is 0");
@@ -140,13 +113,12 @@ contract PrivateRound{
         uint tax = _tokens * initialPercentage[_roundId][msg.sender] / 100;
         initialTokensForFounder[_roundId][_founder] += tax;
         remainingTokensOfInvestor[_roundId][msg.sender] -= initialTokensForFounder[_roundId][_founder];
-        escrowBalance += remainingTokensOfInvestor[_roundId][msg.sender];
     }
 
     mapping(address => uint) public taxedTokens;
     mapping(uint => uint) private withdrawalFee;
-    // 2% tax should be levied on the each transaction
-    function withdrawInitialPercentage(address _tokenContract, address _founderSM, uint _roundId) public {
+    
+    function withdrawInitialPercentage(address _tokenContract, address _founderSM, uint _roundId) public { // 2% tax should be levied on the each transaction
         require(msg.sender != address(0), "The address is not valid or the address is 0");
         FounderLogin founder = FounderLogin(_founderSM);
         require(founder.verifyFounder(msg.sender) == true, "The address is not registered in the 'FounderLogin' contract");
@@ -164,9 +136,7 @@ contract PrivateRound{
 
     mapping(uint => mapping(uint => uint)) private rejectedByInvestor;
     mapping(uint => bool) private projectCancel;
-    // milestone id => founder address => uint
     mapping(uint => mapping(uint => address)) private requestForValidation;
-    // round id => founder address => milestone id.
     mapping(uint => mapping(uint => int)) private milestoneApprovalStatus; // 0 - means default null, 1 - means approves, -1 means rejected.
     mapping(uint => mapping(uint => bool)) private milestoneWithdrawalStatus;
 
@@ -181,7 +151,6 @@ contract PrivateRound{
         require(msg.sender != address(0), "The address is not valid or the address is 0");
         InvestorLogin investor = InvestorLogin(_investorSM);
         require(investor.verifyInvestor(msg.sender) == true, "The address is not registered in the 'InvestorLogin' contract");
-        // whether the milestone id => investor address and milestone mapping, it is already validated or not.
         if(milestoneApprovalStatus[_roundId][_milestoneId] == 1){
             revert("The milestone is already approved");
         }
@@ -197,40 +166,6 @@ contract PrivateRound{
     }
 
     bool private defaultedByFounder;
-    mapping(uint => mapping(uint => bool)) private milestoneStatus;
-    mapping(uint => mapping(address => uint)) private unlockedRead;
-    mapping(uint => mapping(address => uint)) private lockedRead;
-    mapping(uint => mapping(address => uint)) private withdrawnByFounder;
-
-    function batchWithdrawMilestoneTokens(address _founderSM, address _investor, uint _roundId, address _tokenContract) public {
-        require(msg.sender != address(0), "The address is not valid or the address is 0");
-        FounderLogin founder = FounderLogin(_founderSM);
-        require(founder.verifyFounder(msg.sender) == true, "The address is not registered in the 'FounderLogin' contract");
-        uint unlockedAmount = 0;
-        if(initialWithdrawalStatus[_roundId][msg.sender] != true){
-            unlockedAmount = initialTokensForFounder[_roundId][msg.sender];
-        }
-        for(uint i = 0; i < _milestone[_investor][_roundId].length; i++){   
-            uint id = _milestone[_investor][_roundId][i]._num;
-            if(milestoneApprovalStatus[_roundId][id] == 1 && milestoneWithdrawalStatus[_roundId][id] == false){
-                unlockedAmount += (totalTokensOfInvestor[_roundId][_investor] * _milestone[_investor][_roundId][i]._percent)/ 100;
-                milestoneWithdrawalStatus[_roundId][_milestone[_investor][_roundId][i]._num] = true;
-                remainingTokensOfInvestor[_roundId][_investor] -= unlockedAmount;
-            }
-        }
-        if(unlockedAmount > 0){
-            uint tax = (2 * unlockedAmount) / 100;
-            taxedTokens[_tokenContract] += tax;
-            escrowBalance -= unlockedAmount;
-            unlockedAmount -= tax;
-            withdrawalFee[_roundId] += tax;
-            FundLock fl = FundLock(seperateContractLink[_roundId][msg.sender]);
-            ERC20(_tokenContract).transferFrom(address(fl), msg.sender, unlockedAmount);
-            withdrawnByFounder[_roundId][msg.sender] += unlockedAmount;
-        }else{
-            revert("No unlocked tokens to withdraw");
-        } 
-    }
 
     function withdrawIndividualMilestoneByFounder(address _founderSM, address _investor, uint _roundId, uint _milestoneId, uint _percentage, address _tokenContract) public {
         require(msg.sender != address(0), "The address is not valid or the address is 0");
@@ -245,12 +180,10 @@ contract PrivateRound{
         if(unlockedAmount > 0){
             uint tax = (2 * unlockedAmount) / 100;
             taxedTokens[_tokenContract] += tax;
-            escrowBalance -= unlockedAmount;
             unlockedAmount -= tax;
             withdrawalFee[_roundId] += tax;
             FundLock fl = FundLock(seperateContractLink[_roundId][msg.sender]);
             ERC20(_tokenContract).transferFrom(address(fl), msg.sender, unlockedAmount);
-            withdrawnByFounder[_roundId][msg.sender] += unlockedAmount;
         }else{
             revert("No unlocked tokens to withdraw");
         } 
@@ -277,18 +210,17 @@ contract PrivateRound{
             }
             if(lockedAmount > 0){
                 FundLock fl = FundLock(seperateContractLink[_roundId][_founder]);
-                escrowBalance -= lockedAmount;
                 uint tax = (2 * lockedAmount)/ 100;
                 taxedTokens[_tokenContract] += tax;
                 withdrawalFee[_roundId] += tax;
                 lockedAmount -= tax;
+                investorWithdrawnTokens[msg.sender][_roundId] = lockedAmount;
                 ERC20(_tokenContract).transferFrom(address(fl), msg.sender, lockedAmount); 
             }
         }
     }
 
-    mapping(address => mapping(uint => uint)) private investorWithdrawnTokens;
-    // investor add => roundid => withdrawn token
+    mapping(address => mapping(uint => uint)) private investorWithdrawnTokens;  // investor add => roundid => withdrawn token
 
     function batchWithdrawByInvestors(address _investorSM, uint _roundId, address _founder, address _tokenContract) public{
         require(msg.sender != address(0), "The address is not valid or the address is 0");
@@ -313,7 +245,6 @@ contract PrivateRound{
             }
             if(lockedAmount > 0){
                 FundLock fl = FundLock(seperateContractLink[_roundId][_founder]);
-                escrowBalance -= lockedAmount;
                 uint tax = (2 * lockedAmount)/ 100;
                 taxedTokens[_tokenContract] += tax;
                 withdrawalFee[_roundId] += tax;
@@ -328,8 +259,7 @@ contract PrivateRound{
         contractOwner = _newAdmin;
     }
 
-    // All the taxed tokens are there in the contract itself. no instance is created
-    function withdrawTaxTokens(address _tokenContract) public onlyOwner {
+    function withdrawTaxTokens(address _tokenContract) public onlyOwner { // All the taxed tokens are there in the contract itself. no instance is created
         require(msg.sender != address(0), "Invalid address");
         ERC20(_tokenContract).transfer(msg.sender,  taxedTokens[_tokenContract]);
         taxedTokens[_tokenContract] = 0;
@@ -341,14 +271,6 @@ contract PrivateRound{
 
     function milestoneStatusChk(uint roundId, uint milestoneId) public view returns(int){
         return milestoneApprovalStatus[roundId][milestoneId];
-    }
-
-    function milestoneDetails(address _investor, uint _roundId) public view returns(MilestoneSetup[] memory){
-        return _milestone[_investor][_roundId];
-    }
-
-    function getMilestonesDetails(address _investor, uint _roundId) public view returns(MilestoneSetup[] memory){
-        return _milestone[_investor][_roundId];
     }
 
     function getContractAddress(uint _roundId) public view returns(address smartContractAddress){
@@ -376,11 +298,8 @@ contract PrivateRound{
                 uint beforeTax = (totalTokensOfInvestor[_roundId][_investor] * _milestone[_investor][_roundId][i]._percent) / 100;
                 uint tax = (2 * beforeTax)/ 100;
                 withdrawnTokens += beforeTax - tax;
-            // } else if(milestoneApprovalStatus[_roundId][id] != 1){
-            //     lockedTokens += (totalTokensOfInvestor[_roundId][_investor] * _milestone[_investor][_roundId][i]._percent) / 100;
             }
         }
-        // lockedTokens -= investorWithdrawnTokens[_investor][_roundId];
         lockedTokens = totalTokensOfInvestor[_roundId][_investor] - investorWithdrawnTokens[_investor][_roundId] - withdrawnTokens - withdrawalFee[_roundId] - unlockedTokens;
         return(
             unlockedTokens,
@@ -390,13 +309,11 @@ contract PrivateRound{
     }
 
     function investorWithdrawnToken(address _investor, uint _roundId) public view returns(uint investorWithdrawnTokenNumber){
-        uint investorWithdrawTok = investorWithdrawnTokens[_investor][_roundId];
-        return investorWithdrawTok;
+        return investorWithdrawnTokens[_investor][_roundId];
     }
 
     function readTaxFee(uint _roundId) public view returns(uint transactionFee){
-        uint txFee = withdrawalFee[_roundId];
-        return txFee;
+        return withdrawalFee[_roundId];
     }
 
     function milestoneWithdrawStatus(uint _roundId, uint _milestoneId) public view returns(bool){
@@ -409,18 +326,6 @@ contract PrivateRound{
 
     function availableTaxTokens(address _tokenContract) public view returns(uint taxTokens){
         return taxedTokens[_tokenContract];
-    }
-
-    function contractBalance() public view returns(uint escrowBal){
-        return escrowBalance;
-    }
-
-    function defaultedToWithdrawMilestoneTokens() public view returns(bool){
-        return defaultedByFounder;
-    }
-
-    function returnsCurrentBalanceOfInvestor(uint _roundId, address _investor) public view returns(uint investorBalance){
-        return remainingTokensOfInvestor[_roundId][_investor];
     }
 }
 
