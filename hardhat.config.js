d857cfd05f97e0246ff261915987fc8f2071b1f5
Raju Subramanaim <raju.s@agiratech.com>
2022-09-29 21:37:58 +0530

    Added hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
+// require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const {FOUNDER_PVT_KEY,INVESTOR2_PVT_KEY,V1_PVT_KEY,V2_PVT_KEY,V3_PVT_KEY,
+  V4_PVT_KEY,ES_API,ES_API_POLYSCAN,INVESTOR1_PVT_KEY,P_MAINNET,P_MUMBAI,GOERLI} = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks:{
+    polygon_mainnet: {
+      url:P_MAINNET,  
+      accounts: [FOUNDER_PVT_KEY,INVESTOR1_PVT_KEY,INVESTOR2_PVT_KEY,V1_PVT_KEY,V2_PVT_KEY,V3_PVT_KEY,
+        V4_PVT_KEY]
+    },
+    mumbai: {
+      url:P_MUMBAI,   
+      accounts: [FOUNDER_PVT_KEY,INVESTOR1_PVT_KEY,INVESTOR2_PVT_KEY,V1_PVT_KEY,V2_PVT_KEY,V3_PVT_KEY,
+        V4_PVT_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000
+    },
+    goerli: {
+      url:GOERLI,  
+      accounts: [FOUNDER_PVT_KEY,INVESTOR1_PVT_KEY,INVESTOR2_PVT_KEY,V1_PVT_KEY,V2_PVT_KEY,V3_PVT_KEY,
+        V4_PVT_KEY],
+      gas: 2100000,
+      gasPrice: 8000000000
+    }
+  },
+  etherscan : {
+    apiKey : {
+      polygonMumbai:ES_API_POLYSCAN
+    },
+  },
+  mocha: {
+    timeout: 160000
+  }
+};
