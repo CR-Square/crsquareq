e1b926cad65127ec0cbea81bfadfe12fd73151cd
agiratech-leojames <leojames@agiratech.com>
2022-10-12 16:14:29 +0530

    initialize edited

--- contracts/Founder.sol
+++ contracts/Founder.sol
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Founder {
+contract Founder is Initializable, UUPSUpgradeable, OwnableUpgradeable{
     
     mapping(address => bool) private isFounder;
     address[] private pushFounders;
+
+    function initialize() public initializer {
+      ///@dev as there is no constructor, we need to initialise the OwnableUpgradeable explicitly
+       __Ownable_init();
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 
     function addFounder(address _ad) external{
         require(msg.sender == _ad,"Connect same wallet to add founder address");
